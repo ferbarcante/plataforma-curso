d7204bef92ad36885db844ea15eab07ee2bdf0c1
ferbarcante <barcantef@gmail.com>
2025-03-13 11:10:37 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+
+// Dapper
+namespace BaltaDataAcess
+{
+    class Program 
+    {
+
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost,5432;Database=balta;User ID=postgres;Password=123";
+
+            using (var connection = new SqlConnection())
+            {
+                
+            }
+        }
+    }
+}
